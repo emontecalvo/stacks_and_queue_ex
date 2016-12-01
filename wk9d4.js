9868120b974951d5c4021ba959660bb32423249b
emontecalvo <emilymontecalvo@gmail.com>
2016-12-01 11:27:49 -0800

    slight changes

--- wk9d4.js
+++ wk9d4.js
@@ -131,7 +131,14 @@ function squareDancePairing(arr) {
 squareDancePairing(["M", "M", "M", "W", "M", "W", "W", "M"]);
 
 
+// The Ophidian Bank
 
+// At the Ophidian Bank, a single teller serves a long queue of people. 
+// New customers join the end of the queue, and the teller will serve a 
+// customer only if s/he has all the appropriate paperwork.
+//  Write a representation of this queue; 25% of the time (random),
+//   a customer's paperwork isn't quite right, and it's back to the end of the queue. 
+// Show what a few minutes of the bank's lobby would look like.
 
 
 
